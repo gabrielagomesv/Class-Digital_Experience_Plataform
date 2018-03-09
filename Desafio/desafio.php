5a059e79d0d4f77424521cee682a8e6c6db5ff46
Gabriela <gabrielagv@live.com>
2018-03-09 11:50:52 -0300

    Aula 07 - 09/03

---
+++ Desafio/desafio.php
@@ -0,0 +1,7 @@
+// Crie um banco manualmente no phpmyadmin,
+// Crie uma tabela com 4 colunas(id, username, password, email)
+// Realizar a conexão e transações
+// Criar desafio8_create.php
+// Cadastrar 10 usuários
+// Criar desafio8_read.php 
+// Ler os 10 cadastros
