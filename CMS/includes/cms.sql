3a0094d2b0d612d3b3f8b57bf29cf8f6d621caa7
logonrm <logonrm@fiap.com.br>
2018-04-20 11:33:27 -0300

    new sql script added

--- CMS/includes/cms.sql
+++ CMS/includes/cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17-Abr-2018 às 03:48
+-- Generation Time: 20-Abr-2018 às 16:32
 -- Versão do servidor: 5.7.20-log
 -- PHP Version: 7.1.11
 
@@ -42,9 +42,33 @@ INSERT INTO `categoria` (`cat_id`, `cat_nome`) VALUES
 (2, 'Desenvolvimento'),
 (3, 'Empreendedorismo'),
 (4, 'Redes'),
-(5, 'IoT');
+(5, 'IoT'),
+(6, 'bla'),
+(12, 'fdgdfgd'),
+(13, 'fdgdfgd');
 
 -- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(5) NOT NULL,
+  `post_titulo` varchar(255) NOT NULL,
+  `post_autor` varchar(100) NOT NULL,
+  `post_imagem` text NOT NULL,
+  `post_data` date NOT NULL,
+  `post_conteudo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `post_titulo`, `post_autor`, `post_imagem`, `post_data`, `post_conteudo`) VALUES
+(1, 'Aula11 - PHP', 'Gabriela Gomes Vieira', 'cms_post_image1.jpg', '2018-04-20', 'Adipisicing ut commodo anim aliqua. Consequat incididunt dolore adipisicing elit nostrud pariatur ullamco mollit pariatur do occaecat ullamco eiusmod nisi. Id do occaecat est reprehenderit Lorem aute ad pariatur nisi labore.'),
+(2, 'Culinária', 'Maria Braga', 'louro.jpg', '2018-04-20', 'Sint Lorem ad commodo proident amet tempor pariatur consequat. Eu veniam velit nostrud pariatur nulla deserunt. Deserunt excepteur proident voluptate pariatur nostrud ipsum consectetur amet cupidatat excepteur non. Cupidatat laboris incididunt anim minim id magna laborum reprehenderit nisi ipsum mollit cupidatat.\r\n');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +80,11 @@ INSERT INTO `categoria` (`cat_id`, `cat_nome`) VALUES
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`cat_id`);
 
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -65,8 +94,13 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `cat_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
